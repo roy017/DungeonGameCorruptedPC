ffc5fe7a5aa9e9afb1aae08d2f9c2f2dfb9fda49
roy017 <41708735+roy017@users.noreply.github.com>
2020-09-25 17:33:59 -0700

    PcBot detects player and adjust direction

--- CorruptedPC2/Assets/Scripts/Enemies/Enemy_AI_1.cs
+++ CorruptedPC2/Assets/Scripts/Enemies/Enemy_AI_1.cs
@@ -8,12 +8,17 @@ public class Enemy_AI_1 : MonoBehaviour
     private Vector3 roam_Pos;
     private Animator anim;
     private SpriteRenderer sprt;
+    private GameObject gun;
+    private SpriteRenderer sprt_gun;
+    private CircleCollider2D cCol2D;
+    private GameObject player;
 
     public float MIN_Roam_Dist = 10f;
     public float MAX_Roam_Dist = 50f;
     public float enemy_Speed = 1f;
     public bool Alert = false;
     private bool Aware = false;
+    public float detection_Radius = 5f;
 
 
     // Start is called before the first frame update
@@ -23,6 +28,11 @@ public class Enemy_AI_1 : MonoBehaviour
         roam_Pos = roamPosFunc();
         anim = GetComponent<Animator>();
         sprt = GetComponent<SpriteRenderer>();
+        GameObject aim = this.transform.GetChild(0).gameObject;
+        gun = aim.transform.GetChild(0).gameObject;
+        sprt_gun = gun.GetComponent<SpriteRenderer>();
+        cCol2D = GetComponent<CircleCollider2D>();
+        cCol2D.radius = detection_Radius;
     }
 
     // Update is called once per frame
@@ -38,27 +48,52 @@ public class Enemy_AI_1 : MonoBehaviour
         {
             roam_Pos = roamPosFunc();
         }
-
-        anim.SetFloat("Horizontal", roam_Pos.x);
-        anim.SetFloat("Vertical", roam_Pos.y);
-        if((roam_Pos.x - transform.position.x) < 0f)
-            sprt.flipX = true;
-        else
-            sprt.flipX = false;
+        
+        
 
         if (Alert == false)
         {
             anim.SetFloat("Alert", 0f);
+
+            // not sure if this is correct, got to test it
+            anim.SetFloat("Horizontal", roam_Pos.x);
+            anim.SetFloat("Vertical", roam_Pos.y);
+
+            if ((roam_Pos.x - transform.position.x) < 0f)
+                sprt.flipX = true;
+            else
+                sprt.flipX = false;
         }
         else
         {
             if(Aware == false)
             {
                 anim.SetFloat("Alert", 0.5f);
+                sprt_gun.enabled = !sprt_gun.enabled;
                 Aware = true;
             }
             else
                 anim.SetFloat("Alert", 1f);
+            // not sure if this is correct, got to test it
+            anim.SetFloat("Horizontal", (player.transform.position.x - transform.position.x));
+            anim.SetFloat("Vertical", (player.transform.position.y - transform.position.y));
+            if ((player.transform.position.x - transform.position.x) < 0f)
+                sprt.flipX = true;
+            else
+                sprt.flipX = false;
+        }
+
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.tag == "Player")
+        {
+            Alert = true;
+            player = other.gameObject;
+            //Debug.Log("Found Player");
         }
     }
 
